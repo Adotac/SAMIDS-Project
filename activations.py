f842d9fd5b215bf5b0ec68fa7be74b2ccd48865d
Adotac <otacool89@gmail.com>
2022-08-15 20:40:08 +0800

    cnn implementation

---
+++ activations.py
@@ -0,0 +1,50 @@
+import numpy as np
+from layer import Layer
+
+class Activation(Layer):
+    def __init__(self, activation, activation_prime):
+        super().__init__()
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
+
+    def backward(self, output_gradient, learning_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
+
+class Tanh(Activation):
+    def __init__(self):
+        def tanh(x):
+            return np.tanh(x)
+
+        def tanh_prime(x):
+            return 1 - np.tanh(x) ** 2
+
+        super().__init__(tanh, tanh_prime)
+
+class Sigmoid(Activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+
+        super().__init__(sigmoid, sigmoid_prime)
+
+class Softmax(Layer):
+    def forward(self, input):
+        tmp = np.exp(input)
+        self.output = tmp / np.sum(tmp)
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        # This version is faster than the one presented in the video
+        n = np.size(self.output)
+        return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
+        # Original formula:
+        # tmp = np.tile(self.output, n)
+        # return np.dot(tmp * (np.identity(n) - np.transpose(tmp)), output_gradient)
