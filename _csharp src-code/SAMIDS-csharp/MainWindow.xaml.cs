d7b9bcdeea27fe95f3393668e0be11c2cc766785
Adotac <otacool89@gmail.com>
2022-10-10 10:34:48 +0800

    rebase ffrom main

--- _csharp src-code/SAMIDS-csharp/MainWindow.xaml.cs
+++ _csharp src-code/SAMIDS-csharp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace SAMIDS_csharp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
