6f1df2f9038941e6a21c92b76c4738cb27477d68
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-11-28 10:20:27 +0800

    login ands sign up widget

--- _flutter src-code/lib/src/app.dart
+++ _flutter src-code/lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:samids_web_app/src/auth_screen/login.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
@@ -69,11 +70,9 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                
                   default:
-                    return const SampleItemListView();
+                    return const Login();
                 }
               },
             );
