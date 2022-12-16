0debf6383fa80efda17018f2fa5ab86521f6d473
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-12-16 20:13:59 +0800

    Backend API init

---
+++ _csharp src-code/Samids-API/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Samids_API.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set;} = string.Empty;
+        public string FirstName { get; set;} = string.Empty;
+        public string Course { get; set;} = string.Empty; 
+        public Year Year { get; set;}
+    }
+     
+    public enum Year
+        {
+            First,
+            Second,
+            Third,
+            Fourth
+        }
+}
---
+++ _csharp src-code/Samids-API/Models/Subject.cs
@@ -0,0 +1,9 @@
+﻿namespace Samids_API.Models
+{
+    public class Subject
+    {
+        public int SubjectID { get; set; }
+        public string SubjectName { get; set; } = string.Empty;
+        public string SubjectDescription { get; set; } = string.Empty;
+    }
+}
