320550f73ea39cece57091d655667be1759afe4d
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-12-26 17:13:22 +0800

    background fill the app

--- _flutter src-code/lib/src/app.dart
+++ _flutter src-code/lib/src/app.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:samids_web_app/src/sceens/login.dart';
+import 'package:samids_web_app/src/screen/login.dart';
+import 'package:samids_web_app/src/settings/settings_controller.dart';
 
-import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
 /// The Widget that configures your application.
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           // Providing a restorationScopeId allows the Navigator built by the
           // MaterialApp to restore the navigation stack when a user leaves and
           // returns to the app after it has been killed while running in the
