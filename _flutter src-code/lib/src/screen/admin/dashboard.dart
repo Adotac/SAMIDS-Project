46586ad87821eb367b2d83efb9526c1d11a22f45
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-12-27 00:38:36 +0800

    admin dashboard partial layout

--- _flutter src-code/lib/src/screen/admin/dashboard.dart
+++ _flutter src-code/lib/src/screen/admin/dashboard.dart
@@ -1,15 +1,207 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
 
-class AdminDashboard extends StatefulWidget {
-  const AdminDashboard({super.key});
+class AdminDashboard extends StatelessWidget {
+  AdminDashboard({Key? key}) : super(key: key);
 
-  @override
-  State<AdminDashboard> createState() => _AdminDashboardState();
-}
+  final _controller = SidebarXController(selectedIndex: 0, extended: true);
+  final _key = GlobalKey<ScaffoldState>();
 
-class _AdminDashboardState extends State<AdminDashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("hi"),);
+    return Builder(
+      builder: (context) {
+        final isSmallScreen = MediaQuery.of(context).size.width < 600;
+        return Scaffold(
+          key: _key,
+
+          // drawer: ExampleSidebarX(controller: _controller),
+          body: Row(
+            children: [
+              if (!isSmallScreen) ExampleSidebarX(controller: _controller),
+              Expanded(
+                child: Center(
+                  child: _ScreensExample(
+                    controller: _controller,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
+
+class ExampleSidebarX extends StatelessWidget {
+  const ExampleSidebarX({
+    Key? key,
+    required SidebarXController controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final SidebarXController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SidebarX(
+      showToggleButton: false,
+      controller: _controller,
+      theme: SidebarXTheme(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          // color: canvasColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        // hoverColor: scaffoldBackgroundColor,
+        textStyle: TextStyle(color: Colors.black.withOpacity(0.7)),
+        selectedTextStyle: const TextStyle(color: Colors.black),
+        itemTextPadding: const EdgeInsets.only(left: 30),
+        selectedItemTextPadding: const EdgeInsets.only(left: 30),
+        itemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          // border: Border.all(color: canvasColor),
+        ),
+        selectedItemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(
+            color: actionColor.withOpacity(0.37),
+          ),
+          // gradient: const LinearGradient(
+          //   colors: [accentCanvasColor, canvasColor],
+          // ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white,
+            )
+          ],
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+          size: 20,
+        ),
+        selectedIconTheme: const IconThemeData(
+          color: Colors.black,
+          size: 20,
+        ),
+      ),
+      extendedTheme: const SidebarXTheme(
+        width: 200,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+        ),
+      ),
+      footerDivider: divider,
+      headerBuilder: (context, extended) {
+        return SizedBox(
+          // height: 1,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("SAMSS"),
+          ),
+        );
+      },
+      items: [
+        SidebarXItem(
+          icon: Icons.home,
+          label: 'Home',
+          onTap: () {
+            debugPrint('Home');
+          },
+        ),
+        const SidebarXItem(
+          icon: Icons.search,
+          label: 'Search',
+        ),
+        const SidebarXItem(
+          icon: Icons.people,
+          label: 'People',
+        ),
+        const SidebarXItem(
+          icon: Icons.favorite,
+          label: 'Favorites',
+        ),
+      ],
+    );
+  }
+}
+
+class _ScreensExample extends StatelessWidget {
+  const _ScreensExample({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final SidebarXController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          color: Colors.red,
+          height: 60,
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Text(
+              "Home",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+        ),
+        AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            final pageTitle = _getTitleByIndex(controller.selectedIndex);
+            switch (controller.selectedIndex) {
+              case 0:
+                return Text(
+                  "pageTitle",
+                  style: theme.textTheme.headlineSmall,
+                );
+              default:
+                return Text(
+                  pageTitle,
+                  style: theme.textTheme.headlineSmall,
+                );
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
+
+String _getTitleByIndex(int index) {
+  switch (index) {
+    case 0:
+      return 'Home';
+    case 1:
+      return 'Search';
+    case 2:
+      return 'People';
+    case 3:
+      return 'Favorites';
+    case 4:
+      return 'Custom iconWidget';
+    case 5:
+      return 'Profile';
+    case 6:
+      return 'Settings';
+    default:
+      return 'Not found page';
+  }
+}
+
+const primaryColor = Color(0xFF685BFF);
+
+const scaffoldBackgroundColor = Color(0xFF464667);
+const accentCanvasColor = Color(0xFF3E3E61);
+const black = Colors.black;
+final actionColor = const Color(0xFF5F5FA7).withOpacity(0.6);
+final divider = Divider(color: black.withOpacity(0.3), height: 1);
