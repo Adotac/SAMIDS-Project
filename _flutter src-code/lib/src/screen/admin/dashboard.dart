b81dba1a5a0bc0a931dd644d5e000a267de787d8
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2022-12-30 02:07:58 +0800

    linebar widget

--- _flutter src-code/lib/src/screen/admin/dashboard.dart
+++ _flutter src-code/lib/src/screen/admin/dashboard.dart
@@ -10,79 +10,144 @@ class AdminDashboard extends StatelessWidget {
     return Column(
       children: [
         LocalAppBar(pageTitle: pageTitle),
-        Row(
-          children: [
-            userRemarkStats(context),
-            userRemarkStats(context),
-          ],
+        remarkSection(context),
+        Card(
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(15),
+            width: double.maxFinite,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const TitleMediumText(
+                  title: "Attendance by Class",
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    barLine(10, 5),
+                    barLine(10, 7),
+                    barLine(10, 9),
+                    barLine(10, 2),
+                    barLine(10, 10),
+                    barLine(10, 3),
+                    barLine(10, 6),
+                    barLine(10, 4),
+                  ],
+                )
+              ],
+            ),
+          ),
         )
       ],
     );
     ;
   }
 
-  Widget userRemarkStats(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-      ),
+  Padding barLine(max, current) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Container(
-        alignment: Alignment.centerLeft,
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: const Text(
-                "Students",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
+        color: Colors.grey.shade500,
+        height: 200 / max * current,
+        width: 65,
+      ),
+    );
+  }
+
+  Row remarkSection(BuildContext context) {
+    return Row(
+      children: [
+        remarkCard(context),
+        remarkCard(context),
+      ],
+    );
+  }
+
+  Widget remarkCard(BuildContext context) {
+    return Flexible(
+      flex: 1,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: const TitleMediumText(
+                    title: "Students",
+                  )),
+              SizedBox(
+                // width: MediaQuery.of(context).size.width * .10,
+                // width: double.maxFinite,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    dataNumber("55", "Total logs", 5),
+                    dataNumber("11", "Late", 1),
+                    dataNumber("05", "Absent", 1),
+                    dataNumber("04", "Cutting", 1),
+                    dataNumber("35", "On-Time", 1),
+                  ],
                 ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * .39,
-              // width: double.maxFinite,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: dataNumber("55", "Total logs"),
-                  ),
-                  dataNumber("35", "On-Time"),
-                  dataNumber("11", "Late"),
-                  dataNumber("05", "Absent"),
-                  dataNumber("04", "Cutting"),
-                ],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Container dataNumber(number, description) {
-    return Container(
-      // color: Colors.red,
-      width: 80,
-      alignment: Alignment.centerLeft,
-      padding: const EdgeInsets.only(right: 10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            number,
-            style: const TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+  Flexible dataNumber(number, description, flex) {
+    return Flexible(
+      flex: flex,
+      child: Container(
+        // color: Colors.red,
+        // width: 80,
+        alignment: Alignment.centerLeft,
+        // padding: const EdgeInsets.only(right: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              number,
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Text(description),
-        ],
+            Text(description),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TitleMediumText extends StatelessWidget {
+  final String title;
+  const TitleMediumText({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+        ),
       ),
     );
   }
