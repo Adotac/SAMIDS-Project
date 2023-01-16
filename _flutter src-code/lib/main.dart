483800c775c2626d944a3c27657586a7216fe5cf
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2023-01-16 23:57:40 +0800

    Revert "attendance model"
    This reverts commit f20ba085f642085eef7d7c011798d55383cad133.

--- _flutter src-code/lib/main.dart
+++ _flutter src-code/lib/main.dart
@@ -5,20 +5,20 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
-  // ApiController apiController = ApiController();
-  // var future = await apiController.get("Student");
-  // print(future);
+  ApiController apiController = ApiController();
+  var future = await apiController.get("Student");
+  print(future);
 
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  // // Set up the SettingsController, which will glue user settings to multiple
+  // // Flutter Widgets.
+  // final settingsController = SettingsController(SettingsService());
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  // // Load the user's preferred theme while the splash screen is displayed.
+  // // This prevents a sudden theme change when the app is first displayed.
+  // await settingsController.loadSettings();
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  // // Run the app and pass in the SettingsController. The app listens to the
+  // // SettingsController for changes, then passes it further down to the
+  // // SettingsView.
+  // runApp(MyApp(settingsController: settingsController));
 }
