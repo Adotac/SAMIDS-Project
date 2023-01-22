8fe562d95485368120ad897dc11675de96e721e1
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2023-01-22 21:50:36 +0800

    get and post request

--- _flutter src-code/lib/main.dart
+++ _flutter src-code/lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:samids_web_app/src/api/api_controller.dart';
-
+import 'src/api/api_controller.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import './app.dart';
 
 void main() async {
-  // ApiController apiController = ApiController();
-  // var future = await apiController.get("Student");
-  // print(future);
+  ApiController apiController = ApiController();
+  var future = await apiController.get("Student");
+  print(future);
 
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  // final settingsController = SettingsController(SettingsService());
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  // // Load the user's preferred theme while the splash screen is displayed.
+  // // This prevents a sudden theme change when the app is first displayed.
+  // await settingsController.loadSettings();
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  // // Run the app and pass in the SettingsController. The app listens to the
+  // // SettingsController for changes, then passes it further down to the
+  // // SettingsView.
+  // runApp(MyApp(settingsController: settingsController));
 }
