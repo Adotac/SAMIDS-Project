24b0b33952993ccad3d987ddd489f0cfd94ecc40
Martin Erickson Lapetaje <lapetajemartin@gmail.com>
2023-03-05 17:07:42 +0800

    Student attendance view table

---
+++ _flutter src-code/lib/src/screen/student/attendance.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:samids_web_app/src/widgets/app_bar.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../widgets/card_small.dart';
+
+class AdminAttendance extends StatelessWidget {
+  final _textEditingController = TextEditingController();
+  AdminAttendance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        LocalAppBar(pageTitle: "Attendance"),
+        _attendanceBody(context),
+      ],
+    );
+  }
+
+  Widget _attendanceBody(context) {
+    return Expanded(
+        child: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Card(
+        child: Column(children: [
+          SizedBox(height: 18),
+          _searchBar(context),
+          SizedBox(height: 8),
+          Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: DataTable(
+                columns: [
+                  _dataColumn("Student ID"),
+                  _dataColumn("Attendance ID"),
+                  _dataColumn("Room"),
+                  _dataColumn("Subject"),
+                  _dataColumn("Date"),
+                  _dataColumn("Time In"),
+                  _dataColumn("Time Out"),
+                  _dataColumn("Remarks"),
+                ],
+                rows: [for (int i = 0; i < 20; i++) _sampleDataRow(context)],
+              ))
+        ]),
+      ),
+    ));
+  }
+
+  DataRow _sampleDataRow(context) {
+    return DataRow(
+      // ignore: prefer_const_literals_to_create_immutables
+      cells: <DataCell>[
+        DataCell(Text('2019022233')),
+        DataCell(Text('AD1234KA12')),
+        DataCell(Text('BCL3')),
+        DataCell(Text('10023 - Programming 1')),
+        DataCell(Text('March 05')),
+        DataCell(Text('02:12pm')),
+        DataCell(Text('03:16pm')),
+        DataCell(Text('On-Time')),
+      ],
+    );
+  }
+
+  DataColumn _dataColumn(title) {
+    return DataColumn(
+      label: Text(
+        title,
+        style: TextStyle(fontStyle: FontStyle.italic),
+      ),
+    );
+  }
+
+  Container _searchBar(context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      height: 42,
+      width: MediaQuery.of(context).size.width * .30,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(color: Colors.grey, width: 1)),
+      child: TextField(
+        autofocus: true,
+        onSubmitted: (_textEditingController) {
+          if (kDebugMode) {
+            print(_textEditingController.toString());
+          }
+        },
+        controller: _textEditingController,
+        decoration: const InputDecoration(
+          suffixIcon: Icon(Icons.search_outlined),
+          suffixIconColor: Colors.grey,
+          border: InputBorder.none,
+          hintText: 'Search',
+        ),
+      ),
+    );
+  }
+}
