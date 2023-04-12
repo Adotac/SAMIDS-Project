2608ff51717e35f0440e4fd4df42082f24e2e744
Gilben Ferolino <gilbenferolinoz@gmail.com>
2023-04-12 12:10:00 +0800

    Added HttpServices

---
+++ _flutter src-code/lib/src/services/http.services.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class HttpServices{
+ Future<http.Response> get(String url, {Map<String, String>? headers, Map<String, String>? query}) async {
+    final uri = Uri.parse(url).replace(queryParameters: query);
+    final response = await http.get(uri, headers: headers);
+    return response;
+  }
+
+  Future<http.Response> post(String url, {dynamic body, Map<String, String>? headers, Map<String, String>? query}) async {
+    final uri = Uri.parse(url).replace(queryParameters: query);
+    final response = await http.post(
+      uri,
+      headers: headers,
+      body: body != null ? json.encode(body) : null,
+    );
+    return response;
+  }
+
+  Future<http.Response> put(String url, {dynamic body, Map<String, String>? headers, Map<String, String>? query}) async {
+    final uri = Uri.parse(url).replace(queryParameters: query);
+    final response = await http.put(
+      uri,
+      headers: headers,
+      body: body != null ? json.encode(body) : null,
+    );
+    return response;
+  }
+
+  Future<http.Response> patch(String url, {dynamic body, Map<String, String>? headers, Map<String, String>? query}) async {
+    final uri = Uri.parse(url).replace(queryParameters: query);
+    final response = await http.patch(
+      uri,
+      headers: headers,
+      body: body != null ? json.encode(body) : null,
+    );
+    return response;
+  }
+
+  Future<http.Response> delete(String url, {Map<String, String>? headers, Map<String, String>? query}) async {
+    final uri = Uri.parse(url).replace(queryParameters: query);
+    final response = await http.delete(
+      uri,
+      headers: headers,
+    );
+    return response;
+  }
+
+
+
+}
