aec36c4ccedc76ec861dead852f3050da8902b0f
Gilben Ferolino <gilbenferolinoz@gmail.com>
2023-04-14 00:36:46 +0800

    Dashboard stuff

--- _flutter src-code/lib/src/controllers/student_dashboard.controller.dart
+++ _flutter src-code/lib/src/controllers/student_dashboard.controller.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:samids_web_app/src/controllers/student.controller.dart';
+
 import 'package:samids_web_app/src/model/attendance_model.dart';
+import 'package:samids_web_app/src/model/student_model.dart';
 import 'package:samids_web_app/src/model/user_model.dart';
+import 'package:samids_web_app/src/services/DTO/crud_return.dart';
 import 'package:samids_web_app/src/services/attendance.services.dart';
 
 class StudentDashboardController with ChangeNotifier {
@@ -17,7 +19,8 @@ class StudentDashboardController with ChangeNotifier {
   StudentDashboardController get I => GetIt.instance<StudentDashboardController>();
   StudentDashboardController get instance => GetIt.instance<StudentDashboardController>();
 
-  Future<void> getAttendances(DateTime? date, String? room, Remarks? remarks)async{
-    return await AttendanceService.getAll(studentNo: user.)
+  Future<Attendance> getAttendances(DateTime? date, String? room, Remarks? remarks)async{
+    CRUDReturn result = await AttendanceService.getAll(studentNo: student.studentNo);
   }
+  
 }
